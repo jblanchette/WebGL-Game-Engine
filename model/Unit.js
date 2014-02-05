7032ade749b80af001d37afceb41c652c67ceae6
Jeef <jblanch@gmail.com>
2014-02-05 15:11:12 -0500

    Added notes folder, moved some comments into it.  From now on we should write our dev notes here.
    Eventually we'll have a "docs" folder too probably, with outlines for API's and structure.

--- model/Unit.js
+++ model/Unit.js
@@ -4,27 +4,7 @@ G.model.Unit = Class.create(G.model.Entity, {
         this.health = 100;
         this.mana = 100;
         this.addCommand('Idle');
-        /*
-         *  Cursor mode is equal to what the "cursor" is about to do
-         *  Modes:
-         *   0: Default cursor.
-         *      -Right clicking issues "Move" command
-         *      -Left clicking just selects.
-         *
-         *   1: Attack Move.
-         *      -Right clicking cancels the cursor mode.
-         *      -Left clicking issues "AttackMove" command
-         *
-         *   2: Manual Move.
-         *      -Right clicking cancels the cursor mode.
-         *      -Left clicking issues "Move" with 'NoAttack' property
-         *
-         *   3: Spell Cast
-         *      -Right clicking cancels the cursor mode.
-         *      -Left clicking issues "Spell" command
-         *      **NOTE: There are spells that wont change cursor mode
-         *              if they are instant cast. **
-         */
+
 
 
 
