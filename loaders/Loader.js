20f870685aee67e2c03e4b1c1ee7db222e12767f
jblanchette <jeff.blanchette5@gmail.com>
2014-04-02 21:14:03 -0400

    Started Loader class

---
+++ loaders/Loader.js
@@ -0,0 +1,17 @@
+G.component.EntityComponent = Class.create({
+    initialize : function($super) {
+
+    },
+
+    start: function(){
+
+    },
+
+    progress: function(){
+
+    },
+
+    finish: function(){
+        
+    }
+});
